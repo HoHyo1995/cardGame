841f07bcfadb237f9af626128e749a4db84a39ce
Shin-hyoil <wewe1418@naver.com>
2019-12-30 18:11:57 +0900

    카페24호스팅전용 DB주소 주석처리로 추가

--- cardGame/src/service/DBService.java
+++ cardGame/src/service/DBService.java
@@ -8,6 +8,7 @@ class DBService {
 		Connection conn = null;
 		Class.forName("org.mariadb.jdbc.Driver");
 		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/puzzle", "root", "java1234");
+		//Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/wewe1418", "wewe1418", "tjqj4580!");
 		return conn;
 	}
 }
