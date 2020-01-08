c185810f2fe99bba4c66c37d0f191ecdd5fac72e
Shin-hyoil <wewe1418@naver.com>
2020-01-08 19:06:19 +0900

    카페24호스팅 연결을위한 주소 변경

--- cardGame/src/service/DBService.java
+++ cardGame/src/service/DBService.java
@@ -7,8 +7,8 @@ class DBService {
 	public Connection getConnection() throws Exception {
 		Connection conn = null;
 		Class.forName("org.mariadb.jdbc.Driver");
-		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/puzzle", "root", "java1234");
-		//Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/wewe1418", "wewe1418", "tjqj4580!");
+		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/wewe1418", "wewe1418", "tjqj4580!");
+		//conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/puzzle", "root", "java1234");
 		return conn;
 	}
 }
